8084ac3dabdfd586ec513216a03e50caf6d254a7
RetroTrigger <66195617+RetroTrigger@users.noreply.github.com>
2022-01-15 10:25:43 +1000

    update

--- 4-titus-awesome.sh
+++ 4-titus-awesome.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-#install dependencies
-yay -S --noconfirm \
-awesome \
-rofi \
-picom \
-i3lock-fancy \
-xclip \
-ttf-roboto \
-polkit-gnome \
-materia-theme \
-lxappearance \
-flameshot \
-pnmixer \
-network-manager-applet \
-xfce4-power-manager \
-qt5-styleplugins \
-papirus-icon-theme \
-nautilus \
-kitty \
-brave-bin \
-nitrogen \
-arandr \
-lightdm \
-
 #clone the config
 git clone https://github.com/ChrisTitusTech/titus-awesome ~/.config/awesome
 
@@ -32,5 +8,6 @@ mkdir -p ~/.config/rofi
 cp $HOME/.config/awesome/theme/config.rasi ~/.config/rofi/config.rasi
 sed -i '/@import/c\@import "'$HOME'/.config/awesome/theme/sidebar.rasi"' ~/.config/rofi/config.rasi
 
-#enable lightdm
-systemctl enable lightdm
+# Set graphical target and enable sddm at boot
+sudo systemctl enable sddm.service
+sudo systemctl set-default graphical.target
